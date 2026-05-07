e1db0e2a9500cd1b3a53285ca8ec86d52f6ca288
Catalin Niculescu <catalinniculescu20@gmail.com>
2026-05-07 12:00:03 +0200

    feat(logout): remove tokens and cache

--- internal/commands/logout.go
+++ internal/commands/logout.go
@@ -1,7 +1,34 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/catalinlongevai/canvacli/internal/config"
+	"github.com/spf13/cobra"
+)
 
 func NewLogout() *cobra.Command {
-	return &cobra.Command{Use: "logout", Hidden: true, RunE: func(*cobra.Command, []string) error { return nil }}
+	return &cobra.Command{
+		Use:   "logout",
+		Short: "Remove stored credentials and clear cache",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			tokPath, err := config.TokenPath()
+			if err != nil {
+				return err
+			}
+			cachePath, err := config.CacheDBPath()
+			if err != nil {
+				return err
+			}
+			for _, p := range []string{tokPath, cachePath} {
+				if err := os.Remove(p); err != nil && !errors.Is(err, os.ErrNotExist) {
+					return fmt.Errorf("remove %s: %w", p, err)
+				}
+			}
+			fmt.Fprintln(os.Stderr, "Logged out. Local credentials and cache cleared.")
+			return nil
+		},
+	}
 }
